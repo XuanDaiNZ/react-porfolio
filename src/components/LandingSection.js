d069cf0650bad1146c5c249408ff4ca0ad573cb1
XuanDaiNZ <doanxuandainz@gmail.com>
2024-06-12 20:03:52 +1200

    Update avatar URL to use random image from Picsum Photos

--- src/components/LandingSection.js
+++ src/components/LandingSection.js
@@ -8,33 +8,33 @@ const bio2 = "specialised in React";
 
 // Implement the UI for the LandingSection component according to the instructions.
 // Use a combination of Avatar, Heading and VStack components.
-const LandingSection = () => (
-  <FullScreenSection
-    justifyContent="center"
-    alignItems="center"
-    isDarkBackground
-    backgroundColor="#2A4365"
-  >
-    <Avatar 
-    src= "i.pravatar.cc/150?img=7" 
-    boxSize="200px"
-    alt="Avatar"
-    marginBottom="2rem"
-    />
-    <VStack spacing="0.5rem" align="center">
-      <Text fontSize="2xl" fontWeight="bold" paddingBottom={50}>
-        {greeting}
-      </Text>
-      <Heading>
-        {bio1}
-      </Heading>
-      <Heading>
-        {bio2}
-      </Heading>
-    </VStack>
-    
-    
-  </FullScreenSection>
+const LandingSection = () => ( <
+    FullScreenSection justifyContent = "center"
+    alignItems = "center"
+    isDarkBackground backgroundColor = "#2A4365" >
+    <
+    Avatar src = "https://picsum.photos/200"
+    boxSize = "200px"
+    alt = "Avatar"
+    marginBottom = "2rem" /
+    >
+    <
+    VStack spacing = "0.5rem"
+    align = "center" >
+    <
+    Text fontSize = "2xl"
+    fontWeight = "bold"
+    paddingBottom = { 50 } > { greeting } <
+    /Text> <
+    Heading > { bio1 } <
+    /Heading> <
+    Heading > { bio2 } <
+    /Heading> < /
+    VStack >
+
+
+    <
+    /FullScreenSection>
 );
 
 export default LandingSection;
